f7bb72cc6425723645f1cceb2c83565b8341affc
Eugenio Tampieri <eugenio.tampieri@icloud.com>
2019-03-08 11:22:26 +0100

    Initial Commit

---
+++ Game of Life/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Game of Life
+//
+//  Created by Eugenio Tampieri on 08/03/2019.
+//  Copyright © 2019 Eugenio Tampieri. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
